da71a911071b09823ac3a0f3b38dd1347d117520
Luis Vespa <vespasoft@gmail.com>
2017-08-10 00:16:53 -0500

    Se agregan componentes independientes Cars y Prospects para mostrar los datos según el Tab seleccionado en el Home2

---
+++ src/components/prospect/listProspects.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+
+import { StyleSheet, Text, AsyncStorage, RefreshControl, FlatList } from 'react-native';
+
+import { Header, Item, Icon, Input, Button, Container, Content, Spinner,
+   List, ListItem, Thumbnail, Body, Footer, FooterTab } from 'native-base';
+
+import api from '../../utilities/api';
+
+class Prospect extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          error: false,
+          search: '',
+          prospects: [],
+          loading: true,
+          searchWord: '',
+          refreshing: false,
+      }
+  }
+
+  componentDidMount() {
+      let _info = '';
+
+      _info = AsyncStorage.getItem(api.getSessionName())
+      .then( (value) => { return JSON.parse(value) || null; } )
+      .then( (JSON_Value) => {
+          // console.log(JSON_Value);
+          if ( null !== JSON_Value ) {
+              this.fetchData(JSON_Value.dealership_id).done()
+          } else if ( null === JSON_Value ) {
+              this.setState({loading:false});
+              alert('Empty data');
+          }
+      } )
+  }
+
+   // GET REQUEST PARA OBTENER LA LISTA COMPLETA DE PROSPECTOS DEL DEALER ACTUAL
+   async fetchData(dealership_id) {
+       try{
+           // const response = await fetch(URL+dealership_id)
+           const response = api.getProspects(dealership_id);
+           response.then( (json) => {
+               // const json = await response.json()
+               this.setState({
+                   loading: false,
+                   prospects: json
+               })
+               listOfProspects = json;
+               // console.log(listOfCars);
+               // console.log(store.getState());
+           } )
+       }catch(err){
+           this.setState({
+               loading: false,
+               error: true
+           });
+           alert(err);
+       }
+   }
+
+   /* Function: findWord
+      Description: esta funcion busca en el arreglo retornado por el servicio
+   */
+   findWord(word) {
+       const newData = listOfProspects.filter( (item) => {
+           const itemData = item.firstname.toLowerCase();
+           const itemData2 = item.lastname.toLowerCase();
+           const textData  = word.toLowerCase();
+           return (itemData.indexOf(textData) > -1) || (itemData2.indexOf(textData) > -1)
+       } );
+       // console.log(newData);
+       this.setState({prospects:newData});
+   }
+
+  render() {
+      return(
+        <Container>
+          {this.state.loading ? null :
+          <Header style={{marginTop:54}} searchBar rounded>
+              <Item>
+                  <Icon name="ios-search" />
+                  <Input
+                      placeholder="Search"
+                      onChangeText={ (text) => this.findWord( text ) }
+                  />
+                  <Icon name="ios-car" />
+              </Item>
+
+          </Header>
+          }
+
+          <Content>
+            {this.state.loading ? <Spinner style={{marginTop:75}} /> :
+             <FlatList
+               data={this.state.prospects}
+               renderItem={({item}) =>
+               <ListItem >
+                   <Body>
+                       <Text style={{fontWeight: 'bold', marginLeft: 10}}>{item.firstname} {item.lastname}</Text>
+
+                       <Text note style={{marginLeft: 10, }}>{item.address} </Text>
+                       <Text note style={{marginLeft: 10, }}>{item.city} - {item.state} </Text>
+                       {item.cellphone==='' ? null : <Text note style={{marginLeft: 10, }}>phone: {item.cellphone} </Text>}
+                   </Body>
+               </ListItem>}
+               keyExtractor={item => item.sales_id}
+            />
+           }
+
+          </Content>
+
+        </Container>
+      )
+  }
+
+}
+
+export default Prospect
